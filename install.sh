4a50b309543a1d7bbaef52a048e12b9397408c40
wlonestar <wlonestar@163.com>
2022-11-06 16:56:32 +0800

    add process num

--- install.sh
+++ install.sh
@@ -10,53 +10,53 @@ GIT_USER_EMAIL="wlonestar@163.com"
 ZSHRC=./.zshrc
 
 function change_source() {
-  echo "[setup] change apt source list"
-  echo "[setup] backup /etc/apt/sources.list to /etc/apt/sources.list.backup"
+  echo "[setup (1/5)] change apt source list"
+  echo "[setup (1/5)] backup /etc/apt/sources.list to /etc/apt/sources.list.backup"
   sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
   sudo cp $SOURCES_LIST /etc/apt/sources.list
-  echo "[setup] update and upgrade"
+  echo "[setup (1/5)] update and upgrade"
   sudo apt-get update -y
   sudo apt-get upgrade -y
   sudo apt-get autoremove -y
-  echo "[setup] finish!"
+  echo "[setup (1/5)] finish!"
 }
 
 function install_utils() {
-  echo "[setup] install essential applications"
+  echo "[setup (2/5)] install essential applications"
   sudo apt-get install vim neovim wget curl net-tools zsh \
     tree baobab python3-pip tmux -y
-  echo "[setup] finish!"
+  echo "[setup (2/5)] finish!"
 }
 
 function config_neovim() {
-  echo "[setup] config vim and neovim"
+  echo "[setup (3/5)] config vim and neovim"
   cp $VIMRC ~/
   mkdir -p ~/.config
   mkdir -p ~/.config/nvim
   cp $NVIMINIT ~/.config/nvim
-  echo "[setup] finish!"
+  echo "[setup (3/5)] finish!"
 }
 
 function config_git() {
-  echo "[setup] config global git"
+  echo "[setup (4/5)] config global git"
   git config --global credential.helper manager-core
   git config --global user.name $GIT_USER_NAME
   git config --global user.email $GIT_USER_EMAIL
-  echo "[setup] finish!"
+  echo "[setup (4/5)] finish!"
 }
 
 function config_zsh() {
-  echo "[setup] config zsh with ohmyzsh"
+  echo "[setup (5/5)] config zsh with ohmyzsh"
   git clone https://github.com/ohmyzsh/ohmyzsh.git ~/ohmyzsh
   sh ~/ohmyzsh/tools/install.sh
-  echo "[setup] install zsh plugins"
+  echo "[setup (5/5)] install zsh plugins"
   git clone https://github.com/zsh-users/zsh-autosuggestions \
     ~/.oh-my-zsh/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
     ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
   cp $ZSHRC ~/.zshrc
   source ~/.zshrc
-  echo "[setup] finish!"
+  echo "[setup (5/5)] finish!"
 }
 
 function main() {
